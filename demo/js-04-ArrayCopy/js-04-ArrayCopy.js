813a724dd3a6517f67da55413557d9012a078af2
jnshu2040 <liuqiyong0319@live.com>
2017-03-24 19:28:19 +0800

    qiyong,shenkaobei

---
+++ demo/js-04-ArrayCopy/js-04-ArrayCopy.js
@@ -0,0 +1,19 @@
+/**
+ * Created by 1 on 2017/3/24.
+ */
+var a=[1,2,3,4,5];
+var b=[];
+
+
+// var b=a;
+// b=a.slice();
+// b=a.concat();
+// b=JSON.parse(JSON.stringify(a));
+
+
+b[0]=8;
+$("#h1").append(JSON.stringify(a));
+$("#h2").append(JSON.stringify(b));
+
+
+
