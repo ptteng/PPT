74278c857aa3354eb96fcd92bdd7f1b5254f35ee
xiaoyudesu <2547854759xhy@gmail.com>
2017-09-19 19:12:51 +0800

    hi~

---
+++ demo/requirejs/main.js
@@ -0,0 +1,19 @@
+/**
+ * Created by xiaoyudesu on 2017/9/19
+ */
+'use strict';
+
+require.config({
+    paths: {
+        "jquery": "node_modules/jquery/dist/jquery.min",
+        "math": "math",
+        "math2": "math2",
+    }
+})
+
+
+require(['jquery','math','math2'], function ($,math,math2) {
+    $(".a").html("这是被改变的requirejs");
+    alert(math.add(1,1));
+    alert(math2.add(2,2));
+})
